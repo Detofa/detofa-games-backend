2e4754454eec71ef35bab0eee8c77f1de05ec91e
firmin nagnduli <finganduli@gmail.com>
2025-06-04 00:51:53 +0200

    fix: allow all origin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,19 +1,15 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getAllVideos = async (req, res) => {
-    try {
-        const videos = await prisma.video.findMany({
-            include: {
-                user: true
-            }
-        });
-        res.json(videos);
-    } catch (error) {
-        res.status(500).json({ error: 'Error fetching videos' });
-    }
-};
-
-module.exports = {
-  getAllVideos,
+  try {
+    const videos = await prisma.video.findMany({
+      include: {
+        user: true,
+      },
+    });
+    res.json(videos);
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching videos" });
+  }
 };
