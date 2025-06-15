9a5b0992772de8b67f058306ea986736352fa44a
firmin nagnduli <finganduli@gmail.com>
2025-06-16 00:18:43 +0200

    fix:fix get profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -138,25 +138,25 @@ export async function login(req, res) {
 
 export async function getProfile(req, res) {
   console.log("req.userId", req.userId);
-  try {
-    const user = await prisma.user.findUnique({
-      where: { id: req.userId },
-      select: {
-        name: true,
-        email: true,
-        phone: true,
-        city: true,
-        gender: true,
-        parent: true,
-      },
-    });
-    console.log("user", user);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-    console.log("user", user);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+  // try {
+  const user = await prisma.user.findUnique({
+    where: { id: req.userId },
+    select: {
+      name: true,
+      email: true,
+      phone: true,
+      city: true,
+      gender: true,
+      parent: true,
+    },
+  });
+  console.log("user", user);
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
   }
+  console.log("user", user);
+  res.status(200).json(user);
+  // } catch (error) {
+  //   res.status(500).json({ error: "Internal server error" });
+  // }
 }
